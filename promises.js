8af4328ba122e4d81e84bd27916af8494a613aa1
Haseetha-03 <ahaladhinitummala@gmail.com>
2025-08-07 11:16:50 +0530

    intial commit

--- promises.js
+++ promises.js
@@ -19,12 +19,13 @@ function fun3(){
         },1000)
     })
 }
-fun1().then(function(){
-    console.log("Fun-1 Complted")
-    fun2().then(function(){
-        console.log("Fun-2 Completed")
-    })
-    fun3().then(function(){
-        console.log("Fun-3 Completed")
-    })
+// fun1().then(function(){
+//     console.log("Fun-1 Complted")
+//     fun2().then(function(){
+//         console.log("Fun-2 Completed")
+//     })
+//     fun3().then(function(){
+//         console.log("Fun-3 Completed")
+//     })
+
 })
